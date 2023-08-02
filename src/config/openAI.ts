61df8272f2ca31ad3fc36c8422baeaa98bab0ba3
김승환 <prpn97@gmail.com>
2023-08-03 01:06:44 +0900

    rename: config로 변경

--- src/config/openAI.ts
+++
@@ -1,9 +0,0 @@
-import { Configuration, OpenAIApi } from "openai";
-import config from "./index";
-
-const { API_KEY, organization } = config.openai;
-const configuration = new Configuration({
-  apiKey: API_KEY,
-  organization: organization,
-});
-export const openai = new OpenAIApi(configuration);
